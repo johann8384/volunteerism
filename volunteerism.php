24424a66e205a773d36288d7e7d805e32a09c8e9
Jonathan Creasy <jonathan@ghostlab.net>
2013-09-23 00:37:18 -0500

    working on token replacement

--- volunteerism.php
+++ volunteerism.php
@@ -9,13 +9,95 @@
  * License: GPL2
  **/
 
+include('volunteerism_tokens.inc');
+
+// Add the Administration Menu for configuring this plugin
 add_action('admin_menu', 'volunteerism_admin_actions');
 
+// Add the filter which will insert the plugin content into a page or post
+add_filter(‘the_content’,'volunteerism_add_content’);
+
 function volunteerism_admin_actions() {
-  add_options_page("Volunteerism Admin", "Volunteerism Admin", 1, "Volunteerism Admin", "volunteerism_settings");
+  add_options_page('Volunteerism Admin', 'Volunteerism Admin', 'edit_plugins', 'volunteerism_settings', 'volunteerism_settings');
 }
 
 function volunteerism_settings() {
   include('volunteerism_settings.php');
-}  
+}
+
+// Replace defined tokens with plugin content
+function volunteerism_add_content( $content ) {
+
+/*
+Array
+(
+    [0] => Array
+        (
+            [0] => %volunteerism_tk_project_list|foo=5|test%
+            [1] => %volunteerism_tk_listing_all_the_things%
+        )
+
+    [1] => Array
+        (
+            [0] => project_list|foo=5|test
+            [1] => listing_all_the_things
+        )
+
+)
+
+replaces %volunteerism_tk_listing_all_the_things% with the return value of call_user_func('listing_all_the_things');
+replaces %volunteerism_tk_project_list|foo=5|test% with the return value of call_user_func_array('project_list', Array('foo' => 5, 'test' => true));
+
+*/
+
+  preg_match_all('/^%volunteerism_tk_(.+)%$/im', $content, $matches);
+
+  if ($matches === false || ! is_array($matches))
+  {
+    return $content;
+  }
+
+  foreach ( $matches[1] as $possible_callback_function )
+  {
+    $token = 'volunteerism_tk_' . $possible_callback_function;
+    $parts = explode('|', $possible_callback_function);
+
+    $callback_function = $parts[0];
+
+    foreach ($parts as $key => $value)
+    {
+      if ($value == $possible_callback_function)
+      {
+        $callback_function_args = false;
+      }
+      else
+      {
+        $arg_value = explode('=', $value);
+        if ($arg_value[0] == $value)
+        {
+          $callback_function_args[$value] = true;
+        }
+        else
+        {
+          $callback_function_args[$arg_value[0]] = $arg_value[1];
+        }
+      }
+    }
+
+    if (function_exists($callback_function)) {
+      if ($callback_function_args !== false)
+      {
+        $token_replacment = call_user_func_array($callback_function, $callback_function_args);
+      }
+      else
+      {
+        $token_replacement = call_user_func($callback_function)
+      }
+      str_replace($token, $token_replacement, $content));
+    }
+  }
+
+  return $content;
+}
+
 ?>
--- volunteerism_settings.php
+++ volunteerism_settings.php
@@ -1,22 +1,47 @@
+  <?php 
+    if($_POST['volunteerism_hidden'] == 'Y') {
+      //Form data sent
+      $dbhost = $_POST['volunteerism_dbhost'];
+      update_option('volunteerism_dbhost', $dbhost);
+      
+      $dbname = $_POST['volunteerism_dbname'];
+      update_option('volunteerism_dbname', $dbname);
+      
+      $dbuser = $_POST['volunteerism_dbuser'];
+      update_option('volunteerism_dbuser', $dbuser);
+      
+      $dbpwd = $_POST['volunteerism_dbpwd'];
+      update_option('volunteerism_dbpwd', $dbpwd);
+?>
+      <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+<?php
+    } else {
+      //Normal page display
+      $dbhost = get_option('volunteerism_dbhost');
+      $dbname = get_option('volunteerism_dbname');
+      $dbuser = get_option('volunteerism_dbuser');
+      $dbpwd = get_option('volunteerism_dbpwd');
+?>
+  
     <div class="wrap">
-      <?php    echo "<h2>" . __( 'OSCommerce Product Display Options', 'oscimp_trdom' ) . "</h2>"; ?>
+      <?php    echo "<h2>" . __( 'Volunteerism Display Options', 'volunteerism_trdom' ) . "</h2>"; ?>
       
-      <form name="oscimp_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-        <input type="hidden" name="oscimp_hidden" value="Y">
-        <?php    echo "<h4>" . __( 'OSCommerce Database Settings', 'oscimp_trdom' ) . "</h4>"; ?>
-        <p><?php _e("Database host: " ); ?><input type="text" name="oscimp_dbhost" value="<?php echo $dbhost; ?>" size="20"><?php _e(" ex: localhost" ); ?></p>
-        <p><?php _e("Database name: " ); ?><input type="text" name="oscimp_dbname" value="<?php echo $dbname; ?>" size="20"><?php _e(" ex: oscommerce_shop" ); ?></p>
-        <p><?php _e("Database user: " ); ?><input type="text" name="oscimp_dbuser" value="<?php echo $dbuser; ?>" size="20"><?php _e(" ex: root" ); ?></p>
-        <p><?php _e("Database password: " ); ?><input type="text" name="oscimp_dbpwd" value="<?php echo $dbpwd; ?>" size="20"><?php _e(" ex: secretpassword" ); ?></p>
+      <form name="volunteerism_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+        <input type="hidden" name="volunteerism_hidden" value="Y">
+        <?php    echo "<h4>" . __( 'Volunteerism Database Settings', 'volunteerism_trdom' ) . "</h4>"; ?>
+        <p><?php _e("Database host: " ); ?><input type="text" name="volunteerism_dbhost" value="<?php echo $dbhost; ?>" size="20"><?php _e(" ex: localhost" ); ?></p>
+        <p><?php _e("Database name: " ); ?><input type="text" name="volunteerism_dbname" value="<?php echo $dbname; ?>" size="20"><?php _e(" ex: volunteering" ); ?></p>
+        <p><?php _e("Database user: " ); ?><input type="text" name="volunteerism_dbuser" value="<?php echo $dbuser; ?>" size="20"><?php _e(" ex: root" ); ?></p>
+        <p><?php _e("Database password: " ); ?><input type="text" name="volunteerism_dbpwd" value="<?php echo $dbpwd; ?>" size="20"><?php _e(" ex: secretpassword" ); ?></p>
         <hr />
-        <?php    echo "<h4>" . __( 'OSCommerce Store Settings', 'oscimp_trdom' ) . "</h4>"; ?>
-        <p><?php _e("Store URL: " ); ?><input type="text" name="oscimp_store_url" value="<?php echo $store_url; ?>" size="20"><?php _e(" ex: http://www.yourstore.com/" ); ?></p>
-        <p><?php _e("Product image folder: " ); ?><input type="text" name="oscimp_prod_img_folder" value="<?php echo $prod_img_folder; ?>" size="20"><?php _e(" ex: http://www.yourstore.com/images/" ); ?></p>
-        
-      
+        <?php    echo "<h4>" . __( 'Volunteerism Class Settings', 'volunteerism_trdom' ) . "</h4>"; ?>
+        <p><?php _e("Allow Non-Member Signups: " ); ?><input name="allow_signups" value="<?php echo $allow_signups; ?>" type="checkbox"><?php _e(" This option controls whether or not non-registered users may signup for projects." ); ?></p>
+
         <p class="submit">
-        <input type="submit" name="Submit" value="<?php _e('Update Options', 'oscimp_trdom' ) ?>" />
+        <input type="submit" name="Submit" value="<?php _e('Update Options', 'volunteerism_trdom' ) ?>" />
         </p>
       </form>
     </div>
-  
+<?php
+    }
+?>  
